21550c4a47de172994f61da815cb5774441dcce3
OonnaC <oonna.chap@gmail.com>
2019-10-09 15:15:21 +0200

    Utilisation du prompt V2
    Amélioration du programme avec utilisation d'une fonction

---
+++ Javascript/06_PromptJSV2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+                      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
+
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
+	<meta http-equiv="content-language" content="fr" />
+	
+	<title>Test de JavaScript dans un navigateur V2</title>
+
+
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
+	<meta http-equiv="content-language" content="fr" />
+	
+	<script type="text/javascript" src="mesFonctions.js"></script>
+		
+	<title>Test de JavaScript dans un navigateur</title>
+</head>
+
+<body>
+
+	<p>
+		<input type="button"
+			   onclick=" affichePrompt();"
+			   value="Dit bonjour" />
+	</p>	
+	
+	
+</body>
+
+</html>
