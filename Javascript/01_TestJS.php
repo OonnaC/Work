d306bea21dae14a45e5f447031562cc2c814d4e4
OonnaC <oonna.chap@gmail.com>
2019-10-09 13:47:47 +0200

    Premier essai Javascript

---
+++ Javascript/01_TestJS.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+                      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
+
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
+	<meta http-equiv="content-language" content="fr" />
+	
+	<title>Test de JavaScript dans un navigateur</title>
+
+</head>
+
+<body>
+	
+	<h1>Hello world en XHTML2 </h1>
+	 
+	<?php
+		echo "<h1>Hello world en PHP</h1>";
+	?>
+	
+   	<script type="text/javascript">
+		/* ceci est un commentaire Javascript */
+		document.write("<h1>Hello world en Javascript</h1>");
+	</script>
+	
+</html>
