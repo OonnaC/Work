fc93bc0f546afb012737a0b69a06b61d96c8fafe
OonnaC <oonna.chap@gmail.com>
2019-10-09 15:14:24 +0200

    Utilisation du prompt
    Programme permettant de saisir un nom puis d'afficher un message

---
+++ Javascript/05_PromptJSV1.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+                      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
+
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
+	<meta http-equiv="content-language" content="fr" />
+	
+	<title>Test de JavaScript dans un navigateur V2</title>
+
+
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
+	<meta http-equiv="content-language" content="fr" />
+	
+	<script type="text/javascript" src="mesFonctions.js"></script>
+		
+	<title>Test de JavaScript dans un navigateur</title>
+</head>
+
+<body>
+
+	<p>
+		<input type="button"
+			   onclick=" var name = window.prompt('Entrer votre nom : ', 'Nom par d&eacute;faut')
+				       console.log(name);
+				       "
+			   value="Dit bonjour" />
+	</p>	
+	
+	
+</body>
+
+</html>
