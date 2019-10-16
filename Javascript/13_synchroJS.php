cdcb79330962ac893227214fa66b80459dae54ec
OonnaC <oonna.chap@gmail.com>
2019-10-16 11:24:11 +0200

    Synchronisation
    Permet de faire attendre un certain temps entre 2 lignes de code javascript

---
+++ Javascript/13_synchroJS.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+                      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
+
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
+	<meta http-equiv="content-language" content="fr" />
+
+	<script type="text/javascript" src="mesfonctions.js"></script>
+
+	<title>Test de synchronisation</title>
+
+</head>
+
+<body>
+
+   	<script type="text/javascript">
+		/* ceci est un commentaire Javascript */
+		alert("Hello world - Attendre 5 secondes...");
+		sleep(5000);
+		alert("Coucou");
+	</script>
+	
+</body>
+
+</html>
