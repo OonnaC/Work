dc7c3022ef974aead49b58c758f6fc8ffe8a71da
OonnaC <oonna.chap@gmail.com>
2019-10-16 11:38:15 +0200

    Create 14_TimeOutJS.php
    Exécution du code après un interval de temps qui est spécifié.

---
+++ Javascript/14_TimeOutJS.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+                      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
+
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
+	<meta http-equiv="content-language" content="fr" />
+	
+	<script type="text/javascript" src="mesFonctions.js"></script>	
+
+	<title>Test de JavaScript SetTimeOut</title>
+
+</head>
+
+<body>
+	<form>
+		<p>
+ 			<input type="button" value="Affiche un message dans 3 seconds" onclick="timeMsg()" />
+ 		</p>
+ 	</form>
+</body>
+
+</html>
