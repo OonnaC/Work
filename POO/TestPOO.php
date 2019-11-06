b940f15e68494844a939a6ebe7937295d84f8d32
OonnaC <oonna.chap@gmail.com>
2019-11-06 16:32:11 +0100

    Update TestPOO.php
    -Depot, retrait, afficheCompte

--- POO/TestPOO.php
+++ POO/TestPOO.php
@@ -43,6 +43,33 @@ class LeCompte
         self::$compteur++;
     }
     
+    //Méthodes
+    public function depot($lavaleur)
+    {
+        $lesolde = $this->setSoldeActuel;
+        $NouveauSolde = $lesolde + $lavaleur;
+        $this->setSoldeActuel($NouveauSolde);
+    }
+    
+    public function retrait($lavaleur)
+    {
+        //Version Elouan
+        /*$lesolde = $this->setSoldeActuel;
+        $NouveauSolde = $lesolde - $lavaleur;
+        $this->setSoldeActuel($NouveauSolde);*/
+        //Version propre
+        $this->setSoldeActuel($this->getSoldeActuel()-$lavaleur);
+        //Version bourrin
+        //$this->soldeActuel-= $lavaleur;
+    }
+    
+    public function afficheCompte()
+    {
+        echo "<br />".self::TYPE_COMPTE." numero : ".self::$compteur;
+        echo "<br />N° :".$this->getNumeroCompte();
+        echo " - Solde : ".$this->getSoldeActuel()."<br />";
+    }
+    
 }//Fin Classe LeCompte
 
 //Test du constructeur de la classe LeCompte
@@ -55,5 +82,25 @@ echo $monCompte->getNumeroCompte();
 //Deuxième Compte
 $monCompte2 = new LeCompte(456,100);
 echo "<br />".$monCompte2->getSoldeActuel()." ".$monCompte2::$compteur;
+
+//test du constructeur de la classe compte
+$testcompte = new compte(123,500);
+
+//affichage
+echo 'le compte n° '.$testcompte -> getNumeroCompte().' possède sur son compte :'.$testcompte -> getSoldeActuel().' €.';
+echo ' <br />';
+//affichage de la variable statique
+echo 'L`identifiant de ce compte est le  : '.$testcompte::$compteur.'.';
+
+$testcompte->depot(300);
+echo ' <br />';
+echo 'Le nouveau solde est de : '.$testcompte -> getSoldeActuel().' €.';
+
+$testcompte->retrait(200);
+echo ' <br />';
+echo 'Le nouveau solde est de : '.$testcompte -> getSoldeActuel().' €.';
+
+$testcompte->afficheCompte();
+
 
 ?>
