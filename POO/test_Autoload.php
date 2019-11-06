fe44f89ae742006a7e420d8191483c9fabf71b27
OonnaC <oonna.chap@gmail.com>
2019-11-06 17:22:49 +0100

    Update test_Autoload.php

--- POO/test_Autoload.php
+++ POO/test_Autoload.php
@@ -1,11 +1,15 @@
 <?php
 	spl_autoload_register(function ($class) {
-		include 'classes/' . $class . '.class.php';
+		include 'classes/'.$class.'.class.php';
 	});
 	
 	//test de la classe compte
 	$monCompte1 = new compte(123,100);
 	$monCompte1->afficheCompte();
 	echo "<br />";
+	
+	//test de la classe CompteCheque
+	$monCompteCheque = new compteCheque(456,200,5000);
+	$monCompteCheque->afficheCompte();
 
 ?>
