2a036b3612e9f8753d940a19001290061406c59f
OonnaC <oonna.chap@gmail.com>
2019-11-13 17:19:58 +0100

    Update insertuser.php

--- AJAX/crud/php/insertuser.php
+++ AJAX/crud/php/insertuser.php
@@ -2,11 +2,16 @@
 
     include "connectAD.php";
     
+    $login = !empty($_GET['login']);
+    $password = !empty($_GET['password']);
+    $email = !empty($_GET['email']);
+
+    if ($login && $password && $email){
     
+    $sql = "INSERT INTO user (email,login,password) VALUES (".$email.",".$login.",".$password.");";
+    $result = executeSQL($sql);
+    echo '(PHP) OK...';
     
-    
-    
-    
-    
+    }
     
 ?>
