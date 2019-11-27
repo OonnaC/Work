797c42402141bfcc815bfcb7be9891093e6d6046
OonnaC <oonna.chap@gmail.com>
2019-11-27 16:50:13 +0100

    Create listeuser.php

---
+++ AJAX/crud/php/listeuser.php
@@ -0,0 +1,20 @@
+<?php
+    include 'connectAD.php';
+
+    $sql="SELECT login, password, email FROM user;";
+    
+    $results = tableSQL($sql);
+    
+    foreach ($results as $ligne) {
+        //on extrait chaque valeur de la ligne courante
+        $login = $ligne[1];
+        $password = $ligne[2];
+        $email = $ligne[3];
+        echo "<tr>
+                <td>".$login."</td>
+                <td>".$password."</td>
+                <td>".$email."</td>
+               </tr>";
+        //echo $login." ".$password."<br />";
+    }
+?>
