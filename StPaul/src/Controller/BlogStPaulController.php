3e5216aa85f0037c0d3642a2728ef32f87b816ac
OonnaC <oonna.chap@gmail.com>
2019-12-04 09:29:57 +0100

    Creation et modification controller blogstpaul

---
+++ StPaul/src/Controller/BlogStPaulController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogStPaulController extends AbstractController
+{
+    /**
+     * @Route("/blogstpaul", name="blog_st_paul")
+     */
+    public function index()
+    {
+        return $this->render('blog_st_paul/index.html.twig', [
+            'controller_name' => 'BlogStPaulController',
+        ]);
+    }
+    
+    /**
+     * @Route("/", name="home")
+     */
+    public function home() {
+        return $this->render('blog_st_paul/home.html.twig');
+    }
+}
