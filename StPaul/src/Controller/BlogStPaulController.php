cb259abc3a0e301e4bab2a2f8a888fd21b690582
OonnaC <oonna.chap@gmail.com>
2019-12-11 10:04:42 +0100

    Modifications StPaul
    (Re)Création de fichiers

---
+++ StPaul/src/Controller/BlogStPaulController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogStPaulController extends AbstractController
+{
+    /**
+     * @Route("/blogstpaul", name="blog_st_paul")
+     */
+    public function index()
+    {
+        return $this->render('blog_st_paul/index.html.twig', [
+            'controller_name' => 'BlogStPaulController',
+        ]);
+    }
+    
+    /**
+     * @Route("/", name="blog_st_paul")
+     */
+    public function home()
+    {
+        return $this->render('blog_st_paul/index.html.twig');
+    }
+}
